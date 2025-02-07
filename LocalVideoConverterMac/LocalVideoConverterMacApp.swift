7466b989fb4727281c6c89fe8cd40a182b77ba27
LF-137 <an-email@gmail.com>
2025-02-07 21:49:12 +0000

    Initial Commit

---
+++ LocalVideoConverterMac/LocalVideoConverterMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalVideoConverterMacApp.swift
+//  LocalVideoConverterMac
+//
+//  Created by Luis Flacke on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalVideoConverterMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalVideoConverterMacTests/LocalVideoConverterMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalVideoConverterMacTests.swift
+//  LocalVideoConverterMacTests
+//
+//  Created by Luis Flacke on 7/2/25.
+//
+
+import Testing
+@testable import LocalVideoConverterMac
+
+struct LocalVideoConverterMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
